dcca2e19021c3c7ff2c3f53f81f78823ef6bf8cb
João Vitor Dias <joao.dias.santos223@gmail.com>
2021-04-20 18:47:34 -0300

    feat(): create GET method and simple response containing HTML and CSS

--- Methods/GET.py
+++ Methods/GET.py
@@ -1,9 +1,23 @@
 from Message.request import Request
 
 class GET():
+    urlTable = {
+        "/": {"type": "text/html", "filePath": "./assets/index.html"},
+        "/bootstrap.min.css": {"type": "text/css", "filePath": "./assets/bootstrap.min.css"},
+        "/dashboard.css": {"type": "text/css", "filePath": "./assets/dashboard.css"},
+        "/bootstrap-4.0.0/assets/js/vendor/popper.min.js": {"type": "text/css", "filePath": "./assets/bootstrap-4.0.0/assets/js/vendor/popper.min.js"},
+        "/bootstrap-4.0.0/dist/js/bootstrap.min.js": {"type": "text/css", "filePath": "./assets/bootstrap-4.0.0/dist/js/bootstrap.min.js"},
+        "/favicon.ico": {"type": "text/css", "filePath": "./assets/bootstrap-4.0.0/favicon.ico"},
+        
+    }
 
     @staticmethod
     def response(request):
-        print("to aqui")
-        response ={}
+        response = f'''
+HTTP/1.1 200 OK
+Accept: text/html,text/css,text/javascript
+Content-Type: {GET.urlTable[request.URI]["type"]}; charset:utf-8
+'''
+        with open(GET.urlTable[request.URI]["filePath"], 'r', encoding='utf-8') as file:
+            response += "".join(file.readlines())
         return response.encode()
