760944af5dae6a98a570eaa5da4ec9aee7bf57a3
Weverson <unifwpereira@fei.edu.br>
2021-04-24 17:55:37 -0300

    Feat Error page and translated to english

--- methods/GET.py
+++ methods/GET.py
@@ -12,12 +12,12 @@ class GET:
         "/post": {"type": "text/html", "filePath": "./assets/post.html"},
         "/list": {"type": "text/html", "filePath": "./assets/list.html"},
         "/database": {"type": "application/json", "filePath": "./databaseUser/database.json"},
+        "/error": {"type": "text/html", "filePath": "./assets/error.html"},
         "/bootstrap.min.css": {"type": "text/css", "filePath": "./assets/bootstrap.min.css"},
         "/dashboard.css": {"type": "text/css", "filePath": "./assets/dashboard.css"},
         "/popper.min.js": {"type": "text/css", "filePath": "./assets/popper.min.js"},
         "/bootstrap.min.js": {"type": "text/css", "filePath": "./assets/bootstrap.min.js"},
         "/favicon.ico": {"type": "image/x-icon", "filePath": "./assets/bootstrap-4.0.0/favicon.ico"},
-        "/database": {"type": "application/json", "filePath": "./databaseUser/database.json"},
     }
 
     @staticmethod
@@ -25,7 +25,7 @@ class GET:
 
         if request.URI.find('database') != -1 or request.URI in GET.urlTable:
             response: Response = Response(status_code=StatusCode.OK, body="", header={})
-            if request.URI.find('database') != -1 :
+            if request.URI.find('database') != -1:
                 request.URI = '/database'
             with open(GET.urlTable[request.URI]["filePath"], 'r', encoding='utf-8', errors='replace') as file:
                 response.status_code = StatusCode.OK
