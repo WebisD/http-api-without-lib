dd25725174d3f17cf3c55b8ef6ff79bbbb50896f
João Vitor Dias <joao.dias.santos223@gmail.com>
2021-05-07 20:45:35 -0300

    merge fix
    Signed-off-by: João Vitor Dias <joao.dias.santos223@gmail.com>

--- message/Request.py
+++ message/Request.py
@@ -1,24 +1,39 @@
 class Request:
-    def __init__(self, request_message):
-        self.type = request_message['Request-Type']
-        self.URI = request_message['Request-URI']
-        self.http_version = request_message['HTTP-Version']
-        self.host = request_message['Host']
-        self.connection = request_message['Connection']
-        self.user_agent = request_message['User-Agent']
-        self.body = request_message['body']
-        self.valid_content = None if 'Accept' not in request_message else request_message['Accept']
-        self.accept_encoding = request_message['Accept-Encoding']
-        self.accept_language = None if 'Accept-Language' not in request_message else request_message['Accept-Language']
-        self.cookie = None if 'Cookie' not in request_message else request_message['Cookie']
-        self.content_type = None if 'Content-Type' not in request_message else request_message['Content-Type']
-        self.cache = None if 'Cache-Control' not in request_message else request_message['Cache-Control']
-        self.content_length = None if 'Content-Length' not in request_message else request_message['Content-Length']
+    """Responsible for encapsulating a parsed request"""
+
+    def __init__(self, request_message: dict) -> None:
+        """Initializes the Request class instance's attributes
+
+        :param request_message: A dictionary containing a parsed request
+        :returns: None
+
+        """
+        self.type: str = request_message['Request-Type']
+        self.URI: str = request_message['Request-URI']
+        self.http_version: str = request_message['HTTP-Version']
+        self.host: str = request_message['Host']
+        self.connection: str = request_message['Connection']
+        self.user_agent: str = request_message['User-Agent']
+        self.body: str = request_message['body']
+        self.valid_content: str = None if 'Accept' not in request_message else request_message['Accept']
+        self.accept_encoding: str = request_message['Accept-Encoding']
+        self.accept_language: str = None if 'Accept-Language' not in request_message else request_message['Accept-Language']
+        self.cookie: str = None if 'Cookie' not in request_message else request_message['Cookie']
+        self.content_type: str = None if 'Content-Type' not in request_message else request_message['Content-Type']
+        self.cache: str = None if 'Cache-Control' not in request_message else request_message['Cache-Control']
+        self.content_length: str = None if 'Content-Length' not in request_message else request_message['Content-Length']
 
         self.ip = ""
 
-    def setIp(self, ip):
+    def setIp(self, ip: str) -> None:
+        """Responsible for setting a new value to the ip attribute of the Request class instance
+
+        :param ip: A string containing the new ip
+        :returns: None
+
+        """
         self.ip = ip
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Returns a string representation of the Request class instance"""
         return f'{self.type, self.URI, self.http_version, self.host, self.body}...'
--- message/Response.py
+++ message/Response.py
@@ -3,37 +3,52 @@ from message.StatusCode import StatusCode
 
 
 class Response:
-    def __init__(self, status_code, body, header):
+    """Responsible for encapsulating the response headers and body"""
+
+    def __init__(self, status_code: StatusCode, body: str, header: dict) -> None:
+        """Initializes the Response class instance's attributes
+
+        :param status_code: A StatusCode object representing the outcome of the request
+        :param body: A string containing the content of the response
+        :param header: A dictionary containing the headers of the response
+        :returns: None
+
+        """
         self.protocol: str = "HTTP/1.1"
         self.status_code: StatusCode.StatusCode = status_code
         self.body: str = body
         self.headers: dict = header
         self.server: str = "Apache/2.22.14 (Ubuntu-20.04)"
 
-    def encodeResponse(self):
+    def encodeResponse(self) -> bytes:
+        """Responsible for encoding the string representation of the Response class instance"""
+
         response = self.__str__()
 
         return response.encode()
 
+    """TODO: put the image bytes object as an argument instead of storing it in the body to keep consistency across 
+    our code """
     def encodeResponseImages(self):
+        """Responsible for encoding the response along with the image"""
+
         response = ""
         response += self.statusLine() + "\n"
         response += self.headerLine()
         response += self.server + "\n"
         response += "\n"
-        #esponse = response.encode()
 
         return response.encode() + self.body
 
     def statusLine(self):
         """
-        :return: HTTP/1.1 200 OK
+        :returns: A string containing the protocol and the status
         """
         return f'{self.protocol} {self.status_code.value[0]} {self.status_code.value[1]}'
 
     def headerLine(self):
         """
-        :return: Date: Mon, 27 Jul 2009 12:28:53 GMT
+        :returns: Date: Mon, 27 Jul 2009 12:28:53 GMT
                  Last-Modified: Wed, 22 Jul 2009 19:15:56 GMT
                  Content-Length: 88
                  Content-Type: text/html
